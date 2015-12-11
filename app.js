8401c9ef7fddb5bd5c39863d12cfe144f5ac8f5c
mattehardman <mattehardman@gmail.com>
2015-12-11 13:48:03 +0000

    express app with jade stylus

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
+//Module Dependencies
 var express = require('express');
+var morgan = require('morgan');
+var stylus = require('stylus');
+var nib = require('nib');
 
+//Set port number
+var PORTNUM = 3000;
+
+//Initialise Express
 var app = express();
+console.log('Express initialised');
 
+function compile(str,path){
+	return stylus(str)
+	.set('filename',path)
+	.use(nib())
+};
 
-app.get('/',function(req,res){
-  res.send('<h1>This is the homepage</h1>');
+//Set views folder
+app.set('views',__dirname+'/views');
+
+//Initialise Jade
+app.set('view engine','jade');
+console.log('Jade initialised');
+
+//Stylus middleware
+app.use(morgan('dev'));
+app.use(stylus.middleware(
+  {
+    src:__dirname + '/public',
+    compile: compile
+  }
+));
+
+app.use(express.static(__dirname+'/public'));
+
+//Rendex index page
+app.get('/', function(req,res){
+	res.render('index',
+	{
+		title: 'Welcome to my humble abode'
+	}
+	);
+
 });
 
-app.get('/about/:title',function(req,res){
-  res.send('<h1> About '+req.params.title+'</h1>');
-});
-
-
-app.get('/about',function(req,res){
-  res.send('<h1>About us</h1>');
-});
-
-
-
-console.log('Server started');
-app.listen(3000);
-
+app.listen(PORTNUM);
+console.log('Server is now running on localhost:'+PORTNUM);
